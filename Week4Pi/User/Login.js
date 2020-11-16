a984f25af23c4aacbc64a7b82c5abb879f650b55
anh quang <godfeed27@gmail.com>
2020-11-16 23:23:08 +0700

    update

--- Week4Pi/User/Login.js
+++ Week4Pi/User/Login.js
@@ -6,8 +6,11 @@ import {
   StatusBar,
   TextInput,
   TouchableOpacity,
-  TouchableWithoutFeedback
+  TouchableWithoutFeedback,
 } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 
 
@@ -49,15 +52,21 @@ export default class Login extends Component {
     });
   }
     
-
   render() {
     return (
       <View style={styles.container}>
+
         <View style={styles.up}>
           <Text style={styles.title}>Pi</Text>
         </View>
+
         <View style={styles.down}>
+
           <View style={styles.TextInputContainer}>
+            <Icon style={styles.Icon}
+              name='call-outline'
+              size={20}
+            />
             <TextInput
             style={styles.textInput}
             textContentType='emailAddress'
@@ -65,40 +74,42 @@ export default class Login extends Component {
             placeholder='Email'
             keyboardAppearance='default'
             onChangeText={(username) => this.setState({username:username})}
-            >
-            </TextInput>
+            />
           </View>
+
           <View style={styles.TextInputContainer}>
+            <Icon style={styles.Icon}
+            name='lock-closed-outline'
+            size={20}
+            />
+
             <TextInput
             style={styles.textInput}
             placeholder='PassWord'
             secureTextEntry={true}
             onChangeText={(password) => this.setState({password:password})}
-            >
-            </TextInput>
+            />
           </View>
-          <TouchableOpacity>
-            <Text>Quên mật khẩu</Text>
+
+          <TouchableOpacity
+          style={styles.ForgotPasswordButton}
+          >
+            <Text style={{color:'#FF0000'}}>Forgot Password?</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.LoginButton}
-            onPress={this._onSubmit}
+
+          <TouchableOpacity 
+          style={styles.LoginButton}
+          onPress={this._onSubmit}
           >
             <Text style={styles.LoginButtonTitle}>LOGIN</Text>
           </TouchableOpacity>
-          <Text>Hoặc</Text>
-          <View style={{flexDirection:'row'}}>
-            <TouchableOpacity>
-              <Text style={{backgroundColor:'white', marginTop:10}}>FACEBOOK</Text>
-            </TouchableOpacity>
-            <Text>     </Text>
-            <TouchableOpacity>
-              <Text style={{backgroundColor:'white', marginTop:10}}>GOOGLE</Text>
-            </TouchableOpacity>
-          </View>
-          <TouchableOpacity style={styles.LoginButton}>
-            <Text style={styles.LoginButtonTitle}>REGISTER</Text>
+
+          <TouchableOpacity style={styles.SignUpButton}>
+            <Text style={styles.SignUpButtonTitle}>SIGN UP</Text>
           </TouchableOpacity>
+
         </View>
+
       </View>
     )
   }
@@ -129,17 +140,40 @@ const styles = StyleSheet.create({
   title:{
     flex: 1,
     fontSize: 100,
-    color: 'green',
+    color: '#00A79C',
+    fontFamily: 'Rationale',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
   },
   TextInputContainer:{
+    flexDirection:'row',
     marginTop: 10,
     paddingHorizontal: 10,
     borderRadius: 6,
-    backgroundColor: 'rgba(225,225,225,0.5)',
+    backgroundColor: 'rgba(225,225,225,0.0)',
+  },
+  Icon:{
+    backgroundColor:'rgba(74, 184, 177, 0.49)',
+    borderRadius:999,
+    paddingLeft: 5,
+    paddingTop:5,
+    width: 30,
+    height: 30,
+    margin: 10,
+    position:"absolute",
+
   },
   textInput:{
     width: 280,
     height: 45,
+    borderBottomColor: '#C9CFDF',
+    borderBottomWidth: 1,
+    paddingLeft:50
+  },
+  ForgotPasswordButton:{
+    marginTop: 10,
+    alignSelf:'flex-end',
+    marginRight: 60,
   },
   LoginButton:{
     width: 300,
@@ -150,8 +184,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#00A79C',
     marginTop: 10,
   },
+  SignUpButton:{
+    width: 300,
+    height: 45,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    marginTop: 10,
+    marginTop: 20,
+    borderColor:'#00A79C',
+    borderWidth:1,
+    
+  },
   LoginButtonTitle:{
+    fontFamily:'Roboto Slab',
+    fontStyle:'normal',
     fontSize: 18,
-    color: 'black',
+    color: 'white',
+    lineHeight: 21,
+    fontWeight:'bold'
+  },
+  SignUpButtonTitle:{
+    fontFamily:'Roboto Slab',
+    fontStyle:'normal',
+    fontSize: 18,
+    color: '#00A79C',
+    lineHeight: 21,
+    fontWeight:'bold'
   }
 })
